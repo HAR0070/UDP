c4e6e5dc45ad533e2db61afccbe7e6b0c6e5427b
HAR0070 <64668639+HAR0070@users.noreply.github.com>
2021-01-31 03:55:22 +0530

    Update udpserver.py
    UDP-py

--- udpserver.py
+++ udpserver.py
@@ -21,33 +21,26 @@ sock.bind(server_address)
 ttl = struct.pack('b', 1)
 
 # opening a loop to send data
+while True:
+	with open('rc_3.log','r') as reader:   # opening the log file
+		for i in range(0,1000):	
+			send_data = reader.readline(256)  or f'No match for "{query}".'
+		try:
 
-try:
-
-    # Send data to the multicast group
-    print(sys.stderr, 'sending "%s"' % message)
-    with open('rc_3.log','r') as reader:   # opening the log file
-	for i in range(0,1000):	
-		send_data = reader.readline(256)  or f'No match for "{query}".' 			#fetching data line by line, encoding into utf-8
-    		sock.sendto(send_data.encode('utf-8'), address)  # encoding and sending the data
+		# each fetched data is being sent, encoding into utf-8
+    			sock.sendto(send_data.encode('utf-8'), address)  # encoding and sending the data
     	
-
-    # Look for responses from recipient
-    # to confirm if the data reached
-    while True:
-        print(sys.stderr, 'waiting to receive')
-        try:
-        	data, server = sock.recvfrom(16)
-        except socket.timeout:
-            	print(sys.stderr, 'timed out, no more responses')
-            	break
-        else:
-            	print(sys.stderr, 'received "%s" from %s' % (data, server))
-
-finally:
-    	print("####### END OF DRIVE #######")
-    	sock.close()
-
+        	except socket.timeout:
+            		print(sys.stderr, 'timed out, no more responses')
+            		break
+        	else:
+        		data, server = sock.recvfrom(16)
+        		if data == "Closing client "
+            			print(sys.stderr, 'received "%s" from %s' % (data, server))
+				sock.close()
+		finally:
+    			print("####### END OF DRIVE #######")
+    			sock.close()
     
 
     	
